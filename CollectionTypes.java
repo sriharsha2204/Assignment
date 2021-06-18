dca4a61e02121252f4f841250b1207834fcf44f1
sriharsha2204 <85737103+sriharsha2204@users.noreply.github.com>
2021-06-18 10:10:39 +0530

    Add files via upload

---
+++ CollectionTypes.java
@@ -0,0 +1,15 @@
+package collectionexamples;
+
+import java.util.ArrayList;
+import java.util.List;
+public class CollectionTypes {
+public static void main(String args[])
+{
+	 List<String> x = new ArrayList<>();
+	x.add("One");
+	x.add("Two");
+	x.add("One");
+	
+	System.out.println(x);
+}
+}
